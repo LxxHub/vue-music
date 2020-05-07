ef78965287ae11467996a648de4db32c5d2277f3
lxx <lxx@kuaishang.com.cn>
2020-05-07 17:14:56 +0800

    init project

---
+++ src/components/singer-detail/singer-detail.vue
@@ -0,0 +1,86 @@
+<template>
+  <transition appear name="slide">
+    <music-list :songs="songs" :title="title" :bgImage="bgImage"></music-list>
+  </transition>
+</template>
+
+<script text="text/ecmascript-6">
+import { mapGetters } from 'vuex'
+import MusicList from 'components/music-list/music-list'
+import { getSingerDetail } from 'api/singer'
+import { ERR_OK } from 'api/config'
+import { createSong, isValidMusic, processSongsUrl } from 'common/js/song'
+
+export default {
+  computed: {
+    ...mapGetters([
+      'singer'
+    ]),
+    title () {
+      return this.singer.name
+    },
+    bgImage () {
+      return this.singer.avatar
+    }
+  },
+  created () {
+    this._getSingerDetail()
+  },
+  data () {
+    return {
+      songs: []
+    }
+  },
+  components: {
+    MusicList
+  },
+  methods: {
+    _getSingerDetail () {
+      if (!this.singer.id) {
+        this.$router.push('/singer')
+        return
+      }
+      getSingerDetail(this.singer.id).then((res) => {
+        if (res.code === ERR_OK) {
+          processSongsUrl(this._normalizeSongs(res.data.list)).then((songs) => {
+            this.songs = songs
+            console.log(this.songs)
+          })
+        }
+      })
+    },
+    _normalizeSongs (list) {
+      let ret = []
+      list.forEach((item) => {
+        let { musicData } = item
+        if (isValidMusic(musicData)) {
+          ret.push(createSong(musicData))
+        }
+      })
+      return ret
+    }
+  }
+}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+@import '~common/stylus/variable';
+
+.singer-detail {
+  position: fixed;
+  z-index: 100;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: $color-background;
+}
+
+.slide-enter-active, .slide-leave-active {
+  transition: all 0.3s;
+}
+
+.slide-enter, .slide-leave-to {
+  transform: translate3d(100%, 0, 0);
+}
+</style>
